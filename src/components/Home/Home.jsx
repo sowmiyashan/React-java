e1fcb6cf75b1c97d06d913ce3538607b6fdbb62a
sowmiyashan2653@gmail.com <sowmiyashan2653@gmail.com>
2021-01-05 18:21:54 +0530

    Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './Home.css';
 // import { Navbar, NavbarToggler, NavbarBrand, Collapse, Nav, NavItem, Button } from "reactstrap";
 import { NavLink } from 'react-router-dom';
-import { Button, Tooltip, OverlayTrigger, Popover } from 'react-bootstrap'
 
 class Home extends Component {
     constructor(props) {
@@ -11,18 +10,32 @@ class Home extends Component {
     render() {
         return (
             <>
-                <img src="assets/home.webp" className="home_pic"/>
-                <div className="home-over">
-                    <div className="navbar">
-                        <NavLink to="/" className="navlinks">ROOMS</NavLink>
-                        <NavLink to="/" className="navlinks">DINING</NavLink>
-                        <NavLink to="/" className="navlinks">FACILITIES</NavLink>
-                        <NavLink to="/" className="navlinks">GALLERY</NavLink>
-                        <NavLink to="/" className="navlinks">MEETINGS & EVENTS</NavLink>
-                        <NavLink to="/" className="navlinks">CONTACT</NavLink>
-                        <NavLink to="/" className="navlinks">LOGIN</NavLink>
-                    </div>
-                </div>
+                <nav className="navbar navbar-expand-sm bg-light justify-content-center navbar-light">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">ROOMS</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">DINING</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">FACILITIES</a>
+                        </li>
+                        <li className="nav-item">
+                            <img src="assets/logo.png" alt="logo" width="50px" height="50px"/>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">GALLERY</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">MEETINGS &  EVENTS</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">CONTACT US</a>
+                        </li>
+                    </ul>
+                </nav>
+                <img src="assets/home.webp" alt="home" className="home_pic"/>
             </>
 
         );
